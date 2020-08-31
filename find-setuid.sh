d8b6d9dc723856ec397db594fae085713f8a074d
Jason Turley <jasonturley0@gmail.com>
2020-08-31 10:36:20 -0500

    forgot to add "-perm" flag

--- find-setuid.sh
+++ find-setuid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Finds all files the user can run that have the setuid bit enabled. This is a cheesy work-around
-# for when you do not have permission to run sudo -l
+# Finds all files the user can run that have the setuid bit enabled. 
+# This is a cheesy work-around for when you do not have permission to run sudo -l
 
-find / -u=s -type f 2>/dev/null 
+find / -perm -u=s -type f 2>/dev/null 
