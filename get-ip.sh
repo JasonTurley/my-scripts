03d181bb891047dfbb8ab4288eb658fca3f38844
Jason Turley <jasonturley0@gmail.com>
2020-12-14 11:13:41 -0600

    add ability to print range of interfaces

--- get-ip.sh
+++ get-ip.sh
@@ -1,7 +1,21 @@
-#!/bin/bash
-#
-# Gets the device's ip address. Assumes that the NIC interface is eth0.
+#!/bin/sh
+# Prints just the ip addresses for given interfaces.
 
+get_ip()
+{
+    for int in $@
+    do
+	echo -n $int
+	echo -n ': '
+	ip addr show $int | grep -m 1 inet | awk '{print $2}' | cut -d / -f 1
+    done
+}
+
+# Use eth0 if no interfaces are given
 INTERFACE=eth0
 
-ip addr show $INTERFACE | grep -m 1 inet | awk '{print $2}' | cut -d / -f 1
+if [ "$#" -ne 0 ]; then
+    INTERFACE="$@"
+fi
+
+get_ip $INTERFACE
