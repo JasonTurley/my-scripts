72f6a54ad5bbb1d963cb5b71ab1c7ec12ead2eb8
Jason Turley <jasonturley0@gmail.com>
2021-04-11 11:37:58 -0500

    add check for command line argument

--- fix-git-object.sh
+++ fix-git-object.sh
@@ -2,6 +2,12 @@
 # Fixes corrupt git objects directory
 # Source: https://stackoverflow.com/questions/11706215/how-to-fix-git-error-object-file-is-empty
 
+if [ -z "$1" ]; then
+	echo "Usage: $0 </path/to/repo>"
+	echo "Example: $0 ~/my-project"
+	exit
+fi
+
 cd "$1"
 cp -a .git .git-old
 find .git/objects/ -type f -empty | xargs rm
